b1c78372d051aa404cfb1df20744f38309c31ab8
fahadqa900 <fahadqa900@gmail.com>
2024-08-13 20:41:08 +0530

    Committed by fahad for test devops

---
+++ src/Pack1/Test1.java
@@ -0,0 +1,11 @@
+package Pack1;
+
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+	System.out.println("Test First");
+	System.out.println("Test Second");
+	System.out.println("Test Third");
+	}
+}
